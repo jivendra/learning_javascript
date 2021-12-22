4dedfde7cb43690d2859bae600e4ac3ba4f2d487
Jivendra <jivendrasah33@gmail.com>
2021-12-22 23:53:24 +0530

    _

---
+++ jsinfo.js
@@ -0,0 +1,12 @@
+'use strict';
+
+//  #Variables
+
+let message;
+message = 'hello';
+console.log(message);
+let user = 'Jivendra', age = 19;
+const myDob = '6 april 2002';
+let name = 'Jhon';
+let admin = name;
+console.log(admin);
